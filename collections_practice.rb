9f1811cfe3124fd923ae5f178f016841bd9e596a
Bry Cox <carl.cc@gmail.com>
2019-02-03 22:01:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
+def sort_array_asc(int)
+  int.sort {|x,y| x<=>y}
+end
 
+def sort_array_desc(int)
+  int.sort {|x,y| -(x<=>y)}
+end
+
+def sort_array_char_count(array)
+array.sort {|x,y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  arr = []
+  array.each {|x| arr << x.insert(2, "$") && x.slice!(3)}
+  arr
+end
+
+def find_a(array)
+
+end
+
+def sum_array(array)
+  array.inject {|x, y| x + y}
+end
+
+def add_s(array)
+  array.each {|x| x << "s"}
+end
